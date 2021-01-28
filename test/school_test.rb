21c43d07900fab85d2772b543b55eafa4f5964b8
Melanie Tran <melaniexx02@gmail.com>
2021-01-28 09:26:45 -0700

    Complete review

--- test/school_test.rb
+++ test/school_test.rb
@@ -5,28 +5,24 @@ require './lib/school'
 class SchoolTest < Minitest::Test
   #Iteration 1 Tests:
   def test_it_exists
-    skip
     school = School.new('9:00', 7)
 
     assert_instance_of School, school
   end
 
   def test_it_has_start_time
-    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -34,7 +30,6 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
-    skip
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -45,11 +40,33 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_can_calculate_end_time
-    skip
     school1 = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
 
     assert_equal '16:00', school1.end_time
     assert_equal '12:00', school2.end_time
+  end
+
+  #Iteration 3 Tests:
+  def test_check_if_school_is_full_time
+    school1 = School.new('9:00', 7)
+    assert_equal true, school1.is_full_time?
+  end
+
+  def test_it_can_return_list_of_capitalize_student_names
+    school = School.new('9:00', 7)
+
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    expected = ['Aurora', 'Tim', 'Megan']
+    assert_equal expected, school.standard_student_names
+  end
+
+  #Iteration 4 Test
+  def test_convert_to_12_hour_time
+    school = School.new('9:00', 7)
+    assert_equal "4:00", school.convert_end_time_to_clock_time
   end
 end
